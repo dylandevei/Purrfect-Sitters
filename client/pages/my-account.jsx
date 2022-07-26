c52b4678687448c4e2a4606ad93019299a5dec7c
Dylan De Vei <dylan.devei@gmail.com>
2022-07-26 23:56:33 +0000

    fixed an error regarding my-account page

--- client/pages/my-account.jsx
+++ client/pages/my-account.jsx
@@ -2,31 +2,28 @@ import React from 'react';
 import AppContext from '../lib/app-context';
 import Redirect from '../components/redirect';
 import PetList from '../components/pet-list';
+import ProfileList from '../components/profile-list';
 
 export default class MyAccount extends React.Component {
 
   render() {
     if (!this.context.user) return <Redirect to="sign-in" />;
 
-    if (this.context.user) {
+    if (!this.context.SitterProfile) {
       return (
-      <div className="container custom-container card shadow-lg mt-5 px-5">
-        <div className="row">
-          <div className="col">
-            <h1 className='raleway fs-1 mb-3 mt-5 text-center'>Welcome {this.context.user.username} !</h1>
-            <h4 className='raleway fs-6 text-center mb-3'>Your Pets</h4>
+        <div className="container custom-container card shadow-lg mt-5 px-5">
+          <div className="row">
+            <div className="col">
+              <h1 className='raleway fs-1 mb-3 mt-5 text-center'>Welcome {this.context.user.username} !</h1>
+              <h4 className='raleway fs-6 text-center mb-3'>Your Pets</h4>
               <PetList userId={this.context.user.userId} />
-            <div className='lato fs-6 text-center mb-5'>
-              <a href='#pet-form'>Create a Pet Profile</a>
-            </div>
-            <h4 className='raleway fs-6 text-center mb'>Want to Become a Pet Sitter?</h4>
-            <div className='lato fs-6 text-center mb-5'>
-              <a href='#sitter-form'>Create a Sitter Profile</a>
+              <div className='lato fs-6 text-center mb-5'>
+                <a href='#pet-form'>Create a Pet Profile</a>
+              </div>
+              <ProfileList userId={this.context.user.userId} />
             </div>
           </div>
         </div>
-      </div>
-
       );
     }
   }
